7144616b94f5f9f4cbfa644dc2aa1ae5055f4d0f
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-10-12 20:16:38 +0300

    Добавили решение в виде кода + Gitignore

---
+++ Task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arr1 = new string[5] {"mik", "230", "suprem", "classs", "sup"};
+string[] arr2 = new string[arr1.Length];
+
+
+void AnaliticArray(string[] arr1, string[] arr2)
+{
+    int index = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[index] = arr1[i];
+        index++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+AnaliticArray(arr1, arr2);
+PrintArray(arr2);
